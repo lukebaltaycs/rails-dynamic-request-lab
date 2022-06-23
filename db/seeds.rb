41ac051167c1b749ea383317eecf7970e7543db3
Luke Baltay <luke.baltay@yale.edu>
2022-06-23 15:27:36 -0400

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do |student|
+    Student.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
